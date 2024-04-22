f0a847f4f94efbfaa67957dd4529a9841118cd31
Julot <jdesreumaux@e-onsoftware.com>
2024-04-22 11:39:01 +0200

    [ITwin,Renaming] ITwinCesiumRuntime
    renamed the whole module, including headers and all exported symbols
    to avoid conflicts with the official Cesium-Unreal plugin
    - mostly automated through cmake (eon_copy_cesiumruntime_with_renaming)
    - if we have to upgrade cesium-unreal, I think we should reuse the cmake code...)

--- Source/CesiumRuntime/Private/CesiumCreditSystemBPLoader.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright 2020-2021 CesiumGS, Inc. and Contributors
-
-#include "CesiumCreditSystemBPLoader.h"
-#include "UObject/ConstructorHelpers.h"
-
-UCesiumCreditSystemBPLoader::UCesiumCreditSystemBPLoader() {}
