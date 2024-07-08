587a7e325c158c21bf8eb9494767bd5374844bac
Camille Chigot <cchigot@e-onsoftware.com>
2024-07-08 08:47:16 +0200

    fix metadataconversions for uint64

--- Source/ITwinCesiumRuntime/Private/ITwinCesiumMetadataValue.cpp
+++ Source/ITwinCesiumRuntime/Private/ITwinCesiumMetadataValue.cpp
@@ -103,7 +103,7 @@ uint64 UITwinCesiumMetadataValueBlueprintLibrary::GetUnsignedInteger64(
     uint64 DefaultValue) {
   return std::visit(
       [DefaultValue](auto value) -> uint64 {
-        return CesiumGltf::MetadataConversions<uint64, decltype(value)>::convert(
+        return CesiumGltf::MetadataConversions<uint64_t, decltype(value)>::convert(
                    value)
             .value_or(DefaultValue);
       },
