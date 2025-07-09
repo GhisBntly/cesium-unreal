16523f6d85bbc2a11eebf6805ab2edfbfac11cb8
Ghislain Cottat <ghislain.cottat@bentley.com>
2025-07-09 16:15:45 +0200

    AdvViz: Quick hack to avoid using updateViewOffline during movie clip previewing

--- Source/CesiumRuntime/Private/Cesium3DTileset.cpp
+++ Source/CesiumRuntime/Private/Cesium3DTileset.cpp
@@ -59,6 +59,8 @@
 
 FCesium3DTilesetLoadFailure OnCesium3DTilesetLoadFailure{};
 
+/*static*/ bool ACesium3DTileset::AllowCaptureMovieMode = true;
+
 #if WITH_EDITOR
 #include "Editor.h"
 #include "EditorViewportClient.h"
@@ -584,7 +586,7 @@ void ACesium3DTileset::PlayMovieSequencer() {
   this->_beforeMovieLoadingDescendantLimit = this->LoadingDescendantLimit;
   this->_beforeMovieUseLodTransitions = this->UseLodTransitions;
 
-  this->_captureMovieMode = true;
+  this->_captureMovieMode = ACesium3DTileset::AllowCaptureMovieMode;
   this->PreloadAncestors = false;
   this->PreloadSiblings = false;
   this->LoadingDescendantLimit = 10000;
