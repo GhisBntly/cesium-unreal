b95ece4009107ede48c7e58415230152dfdd1d58
Ghislain Cottat <ghislain.cottat@bentley.com>
2025-08-30 13:19:40 +0200

    AdvViz fork: still use our submodule as is for the moment

--- extern/vcpkg-overlays/cesium-native/portfile.cmake
+++ extern/vcpkg-overlays/cesium-native/portfile.cmake
@@ -9,17 +9,17 @@ if(CESIUM_NATIVE_DEPS_ONLY)
   return()
 endif()
 
-vcpkg_from_github(
-        OUT_SOURCE_PATH SOURCE_PATH
-        REPO CesiumGS/cesium-native
-        REF "v${VERSION}"
-        SHA512 c5d6146b2c4ddc299eb72c35791f2f798abfa3662b83a94ad978a698811c9a14d79bff6478bdb74582c6e137a717fe4bcf456c8470c16bb0b962094ec77466e9
-        HEAD_REF vcpkg-pkg
-        )
+# vcpkg_from_github(
+        # OUT_SOURCE_PATH SOURCE_PATH
+        # REPO CesiumGS/cesium-native
+        # REF "v${VERSION}"
+        # SHA512 c5d6146b2c4ddc299eb72c35791f2f798abfa3662b83a94ad978a698811c9a14d79bff6478bdb74582c6e137a717fe4bcf456c8470c16bb0b962094ec77466e9
+        # HEAD_REF vcpkg-pkg
+        # )
+set(SOURCE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../cesium-native")
 
 # Rename sqlite3* symbols to cesium_sqlite3* so they don't conflict with UE's sqlite3,
 # which has a bunch of limitations and is not considered public. The sqlite3 port is built this way.
-
 vcpkg_cmake_configure(
         SOURCE_PATH "${SOURCE_PATH}"
         OPTIONS
